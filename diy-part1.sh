3696f6748902b8f673d1442c49f263feba5df8db
PatchouliTC <1009609373@qq.com>
2022-05-20 14:56:30 +0800

    - update build config

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,3 @@
 
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default 
 echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default 
-git clone https://github.com/fangli/openwrt-vm-tools package/open-vm-tools
